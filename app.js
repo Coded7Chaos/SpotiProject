eb4e13a0362f02863f1ee0292041adca8b27864f
Nick <nicole@digitaltwins.com.bo>
2024-06-03 10:42:57 -0400

    third

--- app.js
+++ app.js
@@ -2,6 +2,57 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://milaeloc:millaeloc@mycluster.st1emty.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster";
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+//const uri = "mongodb+srv://milaeloc:milaeloc@mycluster.st1emty.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster";
+
+
+
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -11,6 +62,10 @@ var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 var ordersRouter = require('./routes/orders');
 
+
+//Mongoose conection
+
+//mongoose.connect("mongodb+srv://milaeloc:milaeloc@mycluster.st1emty.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +79,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Preventing CORS errors
+/*
 
 app.use((req,res,next)=>{
   res.header('Access-Control-Allow-Origin', '*');
@@ -34,6 +90,7 @@ app.use((req,res,next)=>{
   }
   next();
 });
+*/
 
 //conecction to files in routes
 
